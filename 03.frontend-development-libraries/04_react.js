a3f49a5e592a80b5e5f8202d8c4696eee7368596
Fernando <palhares.f@gmail.com>
2024-09-09 21:35:50 -0400

    Adjusted comment format

--- 03.frontend-development-libraries/04_react.js
+++ 03.frontend-development-libraries/04_react.js
@@ -924,7 +924,11 @@ class RenderInput extends React.Component {
 
 
 
-// React components have several special methods that provide opportunities to perform actions at specific points in the lifecycle of a component. These are called lifecycle methods, or lifecycle hooks, and allow you to catch components at certain points in time. This can be before they are rendered, before they update, before they receive props, before they unmount, and so on. Here is a list of some of the main lifecycle methods: componentWillMount() componentDidMount() shouldComponentUpdate() componentDidUpdate() componentWillUnmount() The next several lessons will cover some of the basic use cases for these lifecycle methods.
+/* React components have several special methods that provide opportunities to perform actions at specific points in the lifecycle of a component.
+These are called lifecycle methods, or lifecycle hooks, and allow you to catch components at certain points in time.
+This can be before they are rendered, before they update, before they receive props, before they unmount, and so on.
+Here is a list of some of the main lifecycle methods: componentWillMount() componentDidMount() shouldComponentUpdate() componentDidUpdate() componentWillUnmount()
+The next several lessons will cover some of the basic use cases for these lifecycle methods. */
 
 class MyComponent extends React.Component {
   constructor(props) {
