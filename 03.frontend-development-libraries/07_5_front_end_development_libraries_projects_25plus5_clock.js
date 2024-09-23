4b00d0a826af2e9ca5680a5ec10dd76df2c1714c
Fernando <palhares.f@gmail.com>
2024-09-23 11:19:09 -0300

    HTML and CSS added to Project 4, permalink added to Project 5

---
+++ 03.frontend-development-libraries/07_5_front_end_development_libraries_projects_25plus5_clock.js
@@ -0,0 +1,4 @@
+// Exercise permalink: https://www.freecodecamp.org/learn/front-end-development-libraries/front-end-development-libraries-projects/build-a-25--5-clock
+// Codepen link: https://codepen.io/fernandopa/pen/gOVOgVP
+
+
