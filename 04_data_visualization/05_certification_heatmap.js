8cb5054a04122daa2dc3bef028eb86f510349cf3
Fernando <palhares.f@gmail.com>
2024-10-07 22:47:12 -0400

    Another attempt, still not working. Fixed the axis, but the colors are still not working

--- 04_data_visualization/05_certification_heatmap.js
+++ 04_data_visualization/05_certification_heatmap.js
@@ -7,15 +7,15 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json")
   .then(response => response.json())
   .then(data => {
-      //console.log(data.monthlyVariance);
+    //console.log(data);
   
     // Setup the reference values for the canvas
-    const w = 800;
-    const h = 400;
+    const w = 700;
+    const h = 500;
     const padding = 40;
     const cellsW = w/(2015-1753);
     const cellsH = h/12;
-
+    
     // I'll use this block to work a little bit around the data. The first thing is to find the  thresholds to be used for the legend and color-coding. The math here is self-explanatory
     const variance = data.monthlyVariance.map((d) => d.variance);
       //console.log(variance);
@@ -23,13 +23,38 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
       //console.log("Variation Extent:", variationExtent);
     const varianceRange = variationExtent[1]-variationExtent[0];
     const thresholds = [];
-      //console.log(thresholds);
-
     for (let i = 0; i <= 8; i++) {
       const value = variationExtent[0] + (varianceRange / 8) * i;
-      thresholds.push(Number(value.toFixed(2)));
+      thresholds.push(Number(value.toFixed(3)));
     };  
-      //console.log(thresholds);
+    //console.log(thresholds);
+  
+   // Here I create a function that translates between a month index and its full string name
+    const monthMapping = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December"
+    };
+    const getMonthName = (number) => monthMapping[Math.round(number)] || '';
+  
+  // Here I create a function that takes in a d.variance value and finds the corresponding index in the threshold array
+    const getColorIndex = (variance) => {
+      let colorIndex = 0;
+      while (colorIndex < threshold.length && variance >= threshold[colorIndex])
+      {
+        colorIndex++;
+      }
+      return colorIndex;
+    }; 
 
     // Setup the canvas
     const svg = d3.select("body")
@@ -66,7 +91,10 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
     const colorScale = d3.scaleLinear()
     .domain([0, 8])
     .range(["#0ecbf0", "#f0960e"])
-    .interpolate(d3.interpolateHcl);
+    .interpolate(d3.interpolateRgb);
+    const colorArray = d3.range(9).map(i => colorScale(i));
+    //console.log("ColorScale: ", colorScale);
+    //console.log("ColorArray: ", colorArray)
 
     // Create 9 cells
     for (let i = 0; i < 9; i++) {
@@ -102,7 +130,8 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
       .attr("class", "tick")
       .attr("transform", `translate(${padding}, 0)`)
       .call(d3.axisLeft(yScale)
-            .ticks(12));
+            .ticks(12)
+            .tickFormat(getMonthName));
 
     // Setup the tooltip
     const tooltip = d3.select("body")
@@ -111,26 +140,22 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
 
     // Plot the data
     svg.selectAll("rect")
-      .data(data)
+      .data(data.monthlyVariance)
       .enter()
       .append("rect")
       .attr("class","cell")
-      .attr("data-year",(d) => d.monthlyVariance.year)
-      .attr("data-month",(d) => d.monthlyVariance.month)
-      .attr("data-temp",(d) => 8.66 + d.monthlyVariance.variance)
-      .attr("x", d => xScale(d.monthlyVariance.year))
-      .attr("y", d => yScale(d.monthlyVariance.month))
+      .attr("data-year",(d) => d.year)
+      .attr("data-month",(d) => d.month)
+      .attr("data-temp",(d) => 8.66 + d.variance)
+      .attr("x", d => xScale(d.year))
+      .attr("y", d => yScale(d.month))
       .attr("width", cellsW)
       .attr("height", cellsH)
-      .attr("fill", (d) => {
-        const variance = d.monthlyVariance.variance;
-        const colorIndex = threshold.findIndex(t => variance < t);
-        return colorScale(colorIndex === -1 ? 8 : colorIndex);
-      })
+      .attr("fill", (d) => colorArray[getColorIndex(d.variance)])
             .on("mouseover", function(event, d) {
               tooltip.style("opacity", 0.9)
-                .html(`Temperature: ${8.66 + d.monthlyVariance.variance}, Time: ${d.Time}<br><br>${d.Doping}`)
-                .attr("data-year", d.monthlyVariance.year)
+                .html(`Temperature: ${8.66 + d.variance}`)
+                .attr("data-year", d.year)
                 .style("left", (event.pageX + 10) + "px")
                 .style("top", (event.pageY - 28) + "px");
     })
