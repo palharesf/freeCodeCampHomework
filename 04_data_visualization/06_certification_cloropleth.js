251e0d3294013ac29c4f83c1df7690f7fcbb1624
Fernando <palhares.f@gmail.com>
2024-10-11 17:25:59 -0400

    Exercise completed

--- 04_data_visualization/06_certification_cloropleth.js
+++ 04_data_visualization/06_certification_cloropleth.js
@@ -5,44 +5,63 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 import * as topojson from "https://esm.sh/topojson-client";
 
-// Declare global variables to store the fetched data
-let educationData;
-let countyData;
-
 // Function to fetch both datasets
-function fetchData() {
-  const educationPromise = fetch("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json")
-    .then(response => response.json());
-
-  const countyPromise = fetch("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json")
-    .then(response => response.json());
-
-  // Use Promise.all to fetch both datasets concurrently
-  Promise.all([educationPromise, countyPromise])
-    .then(([education, counties]) => {
-      educationData = education;
-      countyData = counties;
-      
-      // Call a function to process the data or update the UI
-      processData();
-    })
-    .catch(error => console.error('Error fetching data:', error));
-}
+async function fetchData() {
+  try {
+    const [educationResponse, countyResponse] = await Promise.all([
+      fetch("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json"),
+      fetch("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json")
+    ]);
+    
+    const educationData = await educationResponse.json();
+    const countyData = await countyResponse.json();
+    
+    processData(educationData, countyData);
+    }
+  catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}      
 
 // Function to process the data or update the UI
-function processData() {
+function processData(educationData, countyData) {
+  
   // Converting TopoJSON into geoJSON
   const geoCountyData = topojson.feature(countyData,countyData.objects.counties);
   const {features} = geoCountyData;
-  features.forEach((d, educationData) => {
-    // Iterate through the features array, adding the bachelorsOrHigher value to it
+  features.forEach((feat) => {
+    const countyData = educationData.find((edu) => feat.id === edu.fips);
+    feat.educationLevel = countyData.bachelorsOrHigher;
+    feat.countyName = countyData.area_name + ", " + countyData.state;
   });
-  //console.log(features);
     
-  const w = 800;
+  const w = 1000;
   const h = 600;
   const padding = 40;
-
+  const legendSide = h / 25;
+   
+  // Setup the color scale
+  const colorScale = d3.scaleLinear()
+    .domain([0, 3])
+    .range(["#c07272", "#72c083"])
+    .interpolate(d3.interpolateRgb);
+  const colorArray = d3.range(4).map(i => colorScale(i));
+  features.forEach((feat) => {
+    switch (true) {
+      case (feat.educationLevel < 10):
+        feat.color = colorArray[0];
+        break;
+      case (feat.educationLevel < 20):
+        feat.color = colorArray[1];
+        break;
+      case (feat.educationLevel < 30):
+        feat.color = colorArray[2];
+        break;
+      default:
+        feat.color = colorArray [3];
+    }
+  });
+  
   // Setup the canvas
   const svg = d3.select("body")
   .append("svg")
@@ -68,19 +87,58 @@ function processData() {
     .attr("font-size","80%")
     .attr("class", "chart-subtitle")
     .text("% of adults 25+ with bachelor or higher (2010-2014)");
-
+  
+  // Setup the legend
+  const legendText = ["<10%","<20%","<30%",">30%"]
+  
+  const legend = svg.append("g")
+  .attr("id", "legend")
+  .attr("transform", `translate(${(0.8 * w)}, ${0})`);
+  
+  for (let i = 0; i < 4; i++) {
+      legend.append("rect")
+        .attr("x", -legendSide * i)
+        .attr("y", 0)
+        .attr("width", legendSide)
+        .attr("height", legendSide)
+        .attr("fill", colorScale(3-i))
+        .attr("stroke", "gray")
+        .attr("stroke-width", 2);
+    
+      legend.append("text")
+        .attr("x", -legendSide * i)
+        .attr("y", 1.5 * legendSide)
+        .style("font-size","9px")
+        .text(legendText[3-i])
+    }
+  
   // Setup the tooltip
   const tooltip = d3.select("body")
   .append("div")
   .attr("id", "tooltip")
 
   // Plot the data
-  svg.append("path")
-    .datum({type: "FeatureCollection", features: features})
-    .attr("d", d3.geoPath());
-}
+  svg.selectAll("path")
+    .data(features)
+    .enter()
+    .append("path")
+    .attr("d", d3.geoPath())
+    .attr("class", "county")
+    .attr("fill", d => d.color)
+    .attr("data-fips", d => d.id)
+    .attr("data-education", d => d.educationLevel)
+    .on("mouseover", (event, d) => { 
+    tooltip.style("opacity", 0.9)
+      .html(`${d.countyName}: ${d.educationLevel}%`)
+      .attr("data-education", d.educationLevel)
+      .style("left", (event.pageX + 10) + "px")
+      .style("top", (event.pageY - 28) + "px")
+      .style("font-family","Helvetica, sans-serif");
+      })
+    .on("mouseout", () => {
+    tooltip.style("opacity", 0);
+      });  
+};
 
 // Call the fetchData function to start the process
 fetchData();
-
-// Finish tomorrow
