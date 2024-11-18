3bb4b9b41ae9392eb2c84e548536f2707bceda50
Fernando <palhares.f@gmail.com>
2024-11-18 16:56:51 -0500

    Removed test files

--- test.py
+++
@@ -1,9 +0,0 @@
-# Nth Fibonacci number
-def fib(n):
-    """Return the nth Fibonacci number using a naive recursive approach."""
-    if n <= 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib(n-1) + fib(n-2)
