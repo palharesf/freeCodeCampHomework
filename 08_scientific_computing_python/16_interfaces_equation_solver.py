275186f0ce07c2e3ae4fab434b1678c264323c48
Fernando <palhares.f@gmail.com>
2025-01-17 15:59:03 -0300

    File created

---
+++ 08_scientific_computing_python/16_interfaces_equation_solver.py
@@ -0,0 +1,4 @@
+# Exercise link: https://www.freecodecamp.org/learn/scientific-computing-with-python/learn-interfaces-by-building-an-equation-solver/
+
+# An interface is like a blueprint for a class.
+# An interface contains a set of methods and properties that a class should implement.
